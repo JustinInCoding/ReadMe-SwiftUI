3d01e022233c3783a8c7fe110b8595a338152e69
JustinSheng <justin.wang4@icloud.com>
2024-03-30 09:16:49 +0800

    Implement a UIKit Delegate with a Coordinator

--- ReadMe/Views/PHPickerViewController.View.swift
+++ ReadMe/Views/PHPickerViewController.View.swift
@@ -38,8 +38,15 @@ extension PHPickerViewController {
 
 // MARK: - UIViewControllerRepresentable
 extension PHPickerViewController.View: UIViewControllerRepresentable {
+	
+	func makeCoordinator() -> some PHPickerViewControllerDelegate {
+		PHPickerViewController.Delegate(image: $image)
+	}
+	
 	func makeUIViewController(context: Context) -> PHPickerViewController {
-		PHPickerViewController(configuration: .init())
+		let picker = PHPickerViewController(configuration: .init())
+		picker.delegate = context.coordinator
+		return picker
 	}
 	
 	func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
@@ -49,7 +56,26 @@ extension PHPickerViewController.View: UIViewControllerRepresentable {
 
 
 // MARK: - PHPickerViewControllerDelegate
+extension PHPickerViewController.Delegate: PHPickerViewControllerDelegate {
+	func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+		results.first?.itemProvider.loadObject(ofClass: UIImage.self, completionHandler: { image, error in
+			DispatchQueue.main.async {
+				self.image = image as? UIImage
+			}
+		})
+		
+		picker.dismiss(animated: true)
+	}
+}
 
 
 // MARK: - private
-
+private extension PHPickerViewController {
+	final class Delegate {
+		init(image: Binding<UIImage?>) {
+			_image = image
+		}
+		
+		@Binding var image: UIImage?
+	}
+}
